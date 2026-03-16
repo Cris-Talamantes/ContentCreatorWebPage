f5906a6a162bc2434c6ee82bffa8481897a7ee36
Cris-Talamantes <catalamantes1@gmail.com>
2026-03-16 14:46:38 -0600

    Update client.js

--- app/src/client.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-export const supabase = createClient(URL, API_KEY);
-
-
-const URL = 'https://pxqjsgvgvqynpiyqnswf.supabase.co/';
-
-const API_KEY = 'sb_secret_yWyTHgvkTdwVdMEcdYeOBg_0xOkWOPv';
-
-const supabase = createClient(URL, API_KEY);
